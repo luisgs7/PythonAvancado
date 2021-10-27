8d641792431e02e345f42c2e61bc38a93de83a60
luisgs7 <luisgsilva270@gmail.com>
2021-10-27 11:03:18 -0300

    file set.py

---
+++ pythonIntermediário/set.py
@@ -0,0 +1,10 @@
+
+l1 = ['luis', 'gustavo', 'pedro']
+l2 = ['pedro', 'luis', 'gustavo', 'pedro', 'luis', 'gustavo']
+
+
+if set(l1) == set(l2):
+    print('l1 == l2')
+
+else:
+    print('l1 != l2')
