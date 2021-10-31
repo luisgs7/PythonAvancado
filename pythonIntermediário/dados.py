366e820526dbd246bd22f516db7f80e0e515ea9d
luisgs7 <luisgsilva270@gmail.com>
2021-10-31 10:31:11 -0300

    file dados.py

---
+++ pythonIntermediário/dados.py
@@ -0,0 +1,15 @@
+produtos = [
+    {'nome': 'p1', 'preco': 13},
+    {'nome': 'p2', 'preco': 14},
+    {'nome': 'p3', 'preco': 15},
+    {'nome': 'p4', 'preco': 16},
+]
+
+pessoas = [
+    {'nome': 'Luis', 'idade': 20},
+    {'nome': 'Pedro', 'idade': 30},
+    {'nome': 'José', 'idade': 40},
+    {'nome': 'Lucas', 'idade': 50},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
