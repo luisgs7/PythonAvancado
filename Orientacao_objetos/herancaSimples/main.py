a12b1d9797778d021e81688c0712f988fcf21e1c
luisgs7 <luisgsilva270@gmail.com>
2021-11-26 12:19:24 -0300

    class HerancaSimples

---
+++ Orientacao_objetos/herancaSimples/main.py
@@ -0,0 +1,13 @@
+from classe import Pessoa, Aluno, Cliente
+
+
+c1 = Cliente('Luis', 70)
+c1.falar()
+c1.comprar()
+
+a1 = Aluno('Pedro', 20)
+a1.falar()
+a1.estudar()
+
+p1 = Pessoa('João', 50)
+p1.falar()
