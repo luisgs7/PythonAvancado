db7e3a7991a449853c436ba0bf24602907101128
luisgs7 <luisgsilva270@gmail.com>
2021-11-26 12:19:48 -0300

    class HerancaMultipla

---
+++ Orientacao_objetos/herancaMultipla/main.py
@@ -0,0 +1,11 @@
+from smartphone import Smartphone
+
+
+smartphone = Smartphone('Nokia')
+smartphone.conectar()
+smartphone.desligar()
+smartphone.desligar()
+smartphone.ligar()
+smartphone.desconectar()
+smartphone.conectar()
+smartphone.desligar()
