0db0690b1d016ea352524755f4ae34b87d7befd3
luisgs7 <luisgsilva270@gmail.com>
2021-11-26 12:19:02 -0300

    class sobreposicao

---
+++ Orientacao_objetos/sobreposicao/classes.py
@@ -0,0 +1,28 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.nomeclasse = self.__class__.__name__
+
+    def falar(self):
+        print(f'{self.nomeclasse} falando...')
+
+
+class Cliente(Pessoa):
+    def comprar(self):
+        print(f'{self.nomeclasse} comprando...')
+
+    def falar(self):
+        print('Estou em CLIENTE.')
+
+
+class ClienteVip(Cliente):
+    def __init__(self, nome, idade, sobrenome):
+        super().__init__(nome, idade)
+        self.sobrenome = sobrenome
+
+    def falar(self):
+        Pessoa.falar(self)
+        super()
+        print(f'{self.nome} {self.sobrenome}')
+        
---
+++ Orientacao_objetos/sobreposicao/main.py
@@ -0,0 +1,10 @@
+from classes import Pessoa, ClienteVip, Cliente
+
+
+c1 = Cliente('Luis', 70)
+c1.falar()
+c1.comprar()
+
+c2 = ClienteVip('Pedro', 80, 'Francisco')
+c2.falar()
+c2.comprar()
