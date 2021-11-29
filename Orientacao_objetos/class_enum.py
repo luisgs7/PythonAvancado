8a35aa371fb966c906cc8d0ba81faf4891d7ae98
luisgs7 <luisgsilva270@gmail.com>
2021-11-29 18:09:57 -0300

    class enum

---
+++ Orientacao_objetos/class_enum.py
@@ -0,0 +1,26 @@
+from enum import Enum, auto
+
+
+class Directions(Enum):
+    right = auto()
+    left = auto()
+    up = auto()
+    down = auto()
+
+
+def move(direction):
+    if not isinstance(direction, Directions):
+        raise ValueError('Cannot move in this direction')
+
+    return f'Moving {direction.name}'
+
+
+if __name__ == '__main__':
+    print(move(Directions.right))
+    print(move(Directions.left))
+    print(move(Directions.up))
+    print(move(Directions.down)+'\n')
+
+
+    for direction in Directions:
+        print(direction.name)
