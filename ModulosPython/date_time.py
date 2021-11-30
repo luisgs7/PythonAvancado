eaf5fbe246fb8bc8b637411732695d0d3d805c3e
luisgs7 <luisgsilva270@gmail.com>
2021-11-30 11:48:11 -0300

    file date_time

---
+++ ModulosPython/date_time.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+d1 = datetime.strptime('20/04/1990 20:00:30', '%d/%m/%Y %H:%M:%S')
+d2 = datetime.strptime('22/04/1990 20:00:50', '%d/%m/%Y %H:%M:%S')
+
+print(d2.second)
+print(d2 < d1)
+print(d1 < d2)
+print(d1.timestamp())
+
+d3 = datetime.fromtimestamp(640652430.0)
+print(d3)
+print(d3.day)
+print(d3.second)
+print(d3.microsecond)
